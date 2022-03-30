d0eb0a9bec04684a551220b646be4021a7633c02
Adam Turner <act@adamturner.dev>
2022-03-30 08:50:35 -0700

    Create plugin entrypoint

---
+++ hrswp-employee-recognition.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Plugin Name: HRSWP Employee Recognition
+ * Plugin URI: https://github.com/washingtonstateuniversity/hrswp-employee-recognition
+ * Update URI: https://api.github.com/repos/washingtonstateuniversity/hrswp-employee-recognition/releases/latest
+ * Description: WSU HRS plugin that helps to manage the employee recognition progmam.
+ * Version: 0.1.0-alpha.1
+ * Author: Adam Turner
+ * Author URI: https://hrs.wsu.edu/
+ * License: GPLv3 or later
+ * Text Domain: hrswp-employee-recognition
+ * Requires PHP: 7.3
+ * Requires at least: 5.9
+ * Tested up to: 5.9.2
+ *
+ * @package EmployeeRecognition
+ */
+
+namespace Hrswp\EmployeeRecognition;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
