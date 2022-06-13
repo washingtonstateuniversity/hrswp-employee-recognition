93c4075c1b3c981887e8359f48f8dcf634ccd86e
Adam Turner <act@adamturner.dev>
2022-06-13 10:24:22 -0700

    Fix merge error

--- src/blocks/index.js
+++ src/blocks/index.js
@@ -11,16 +11,12 @@ import * as erAwardMetaYear from './er-award-meta-year';
 import * as erAwardInventory from './er-award-inventory';
 import * as erAwardsList from './er-awards-list';
 
-<<<<<<< HEAD
 const erAwardBlocks = [
 	erAwardDescription,
 	erAwardMetaYear,
 	erAwardInventory,
 	erAwardsList,
 ];
-=======
-const erAwardBlocks = [ erAwardDescription, erAwardMetaYear, erAwardInventory ];
->>>>>>> develop
 
 const registerBlock = ( block ) => {
 	if ( ! block ) {
